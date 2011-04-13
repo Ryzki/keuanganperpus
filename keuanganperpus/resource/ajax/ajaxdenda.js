7b418e3ce54c3cd06a0f11d76850d8f821105575
diarez77 <diarez77@gmail.com>
2011-04-13 07:59:41 +0000

    okkkk

--- keuanganperpus/resource/ajax/ajaxdenda.js
+++ keuanganperpus/resource/ajax/ajaxdenda.js
@@ -1,146 +1,200 @@
-   function dosearch(xAwal){ 
-   xSearch =""; 
+function dosearch(xAwal){ 
+    xSearch ="";
     try 
-        {             if ($("#edSearch").val()!=""){ 
-              xSearch = $("#edSearch").val();
+    {
+        if ($("#edSearch").val()!=""){
+            xSearch = $("#edSearch").val();
         } 
-         }catch(err){ 
-          xSearch =""; 
-         } 
-   if (typeof(xSearch) =="undefined"){ 
-     xSearch =""; 
+    }catch(err){
+        xSearch ="";
+    }
+    if (typeof(xSearch) =="undefined"){
+        xSearch ="";
     } 
-  $(document).ready(function(){ 
-  $.ajax({ 
-          url: getBaseURL()+"index.php/ctrdenda/search/",
-          data: "xAwal="+xAwal+"&xSearch="+xSearch, 
-          cache: false, 
-          dataType: 'json', 
-          type: 'POST', 
-       success: function(json){ 
-           $("#tabledata").html(json.tabledata); 
-          }, 
-        error: function (xmlHttpRequest, textStatus, errorThrown) { 
-         start = xmlHttpRequest.responseText.search("<title>") + 7; 
-          end  = xmlHttpRequest.responseText.search("</title>"); 
-       errorMsg = " error on search "; 
-          if (start > 0 && end > 0) 
-             alert("Rangerti "+errorMsg + "  [" + xmlHttpRequest.responseText.substring(start, end) + "]"); 
-          else 
-              alert("Error juga"+errorMsg);  
-         } 
-         }); 
-       }); 
- } 
+    $(document).ready(function(){
+        $.ajax({
+            url: getBaseURL()+"index.php/ctrdenda/search/",
+            data: "xAwal="+xAwal+"&xSearch="+xSearch,
+            cache: false,
+            dataType: 'json',
+            type: 'POST',
+            success: function(json){
+                $("#tabledata").html(json.tabledata);
+            },
+            error: function (xmlHttpRequest, textStatus, errorThrown) {
+                start = xmlHttpRequest.responseText.search("<title>") + 7;
+                end  = xmlHttpRequest.responseText.search("</title>");
+                errorMsg = " error on search ";
+                if (start > 0 && end > 0)
+                    alert("Rangerti "+errorMsg + "  [" + xmlHttpRequest.responseText.substring(start, end) + "]");
+                else
+                    alert("Error juga"+errorMsg);
+            }
+        });
+    });
+} 
 
-   function doedit(edidx){ 
- $(document).ready(function(){ 
- $.ajax({ 
-    url: getBaseURL()+"index.php/ctrdenda/editrec/",
-   data: "edidx="+edidx, 
-  cache: false, 
- dataType: 'json', 
-     type: 'POST', 
-  success: function(json){ 
-       $("#edidx").val(json.idx); 
-       $("#edNoIdentitas").val(json.NoIdentitas); 
-       $("#edNama").val(json.Nama); 
-       $("#edidJenisAnggota").val(json.idJenisAnggota); 
-       $("#edAlamat").val(json.Alamat); 
-       $("#edKota").val(json.Kota); 
-       $("#edkodepos").val(json.kodepos); 
-       $("#edNotelp").val(json.Notelp); 
-       $("#edemail").val(json.email); 
-     }, 
- error: function (xmlHttpRequest, textStatus, errorThrown) { 
- start = xmlHttpRequest.responseText.search("<title>") + 7; 
-     end = xmlHttpRequest.responseText.search("</title>"); 
- errorMsg = "OnEdit "; 
- if (start > 0 && end > 0)  alert("On Edit "+errorMsg + "  [" + xmlHttpRequest.responseText.substring(start, end) + "]"); 
-  else 
- alert("Error juga "+errorMsg); 
- } 
- }); 
- }); 
- } 
- function doClear(){ 
- $(document).ready(function(){ 
- $("#edidx").val("0"); 
- $("#edNoIdentitas").val(""); 
- $("#edNama").val(""); 
- $("#edidJenisAnggota").val(""); 
- $("#edAlamat").val(""); 
- $("#edKota").val(""); 
- $("#edkodepos").val(""); 
- $("#edNotelp").val(""); 
- $("#edemail").val(""); 
-  }); 
- } 
-         function dosimpan(){ 
-         $(document).ready(function(){ 
-           $.ajax({ 
-                 url: getBaseURL()+"index.php/ctrdenda/simpan/",
-   data: "edidx="+$("#edidx").val()+"&edNoIdentitas="+$("#edNoIdentitas").val()+"&edNama="+$("#edNama").val()+"&edidJenisAnggota="+$("#edidJenisAnggota").val()+"&edAlamat="+$("#edAlamat").val()+"&edKota="+$("#edKota").val()+"&edkodepos="+$("#edkodepos").val()+"&edNotelp="+$("#edNotelp").val()+"&edemail="+$("#edemail").val(), 
-                 cache: false, 
-                 dataType: 'json', 
-                 type: 'POST', 
-                 success: function(msg){ 
-                     doClear(); 
-                     dosearch('-99'); 
-                 }, 
-               error: function (xmlHttpRequest, textStatus, errorThrown) { 
-                     start = xmlHttpRequest.responseText.search("<title>") + 7; 
-                     end = xmlHttpRequest.responseText.search("</title>"); 
-                     errorMsg =  " On Simpan "; 
-                     if (start > 0 && end > 0) 
-                         alert("Rangerti "+errorMsg + "  [" + xmlHttpRequest.responseText.substring(start, end) + "]"); 
-                     else 
-                         alert("Error juga "+errorMsg); 
-               } 
-           }); 
-         }); 
-         } 
+function doedit(edidx){ 
+    $(document).ready(function(){
+        $.ajax({
+            url: getBaseURL()+"index.php/ctrdenda/editrec/",
+            data: "edidx="+edidx,
+            cache: false,
+            dataType: 'json',
+            type: 'POST',
+            success: function(json){
+                $("#edidx").val(json.idx);
+                $("#edidsparta").val(json.iddendasparta);
+                $("#edNoIdentitas").val(json.NoIdentitas);
+                $("#edDendaSparta").val(json.nominalpersatuan);
+                $("#edDenda").val(json.nominaldenda);
+            },
+            error: function (xmlHttpRequest, textStatus, errorThrown) {
+                start = xmlHttpRequest.responseText.search("<title>") + 7;
+                end = xmlHttpRequest.responseText.search("</title>");
+                errorMsg = "OnEdit ";
+                if (start > 0 && end > 0)  alert("On Edit "+errorMsg + "  [" + xmlHttpRequest.responseText.substring(start, end) + "]");
+                else
+                    alert("Error juga "+errorMsg);
+            }
+        });
+    });
+} 
+function doClear(){ 
+    $(document).ready(function(){
+        $("#edidx").val("0");
+        $("#edidsparta").val("");
+        $("#edNoIdentitas").val("");
+        $("#edDendaSparta").val("");
+        $("#edDenda").val();
+    });
+} 
+function dosimpan(){ 
+    $(document).ready(function(){
+        $.ajax({
+            url: getBaseURL()+"index.php/ctrdenda/simpan/",
+            data: "edidx="+$("#edidx").val()+
+                "&edtgldenda="+$("#edtgldenda").val()+
+                "&edidsparta="+$("#edidsparta").val()+
+                "&edNoIdentitas="+$("#edNoIdentitas").val()+
+                "&edDendaSparta="+$("#edDendaSparta").val()+
+                "&edDenda="+$("#edDenda").val(),
+            cache: false,
+            dataType: 'json',
+            type: 'POST',
+            success: function(msg){
+                doClear();
+                dosearch('-99');
+            },
+            error: function (xmlHttpRequest, textStatus, errorThrown) {
+                start = xmlHttpRequest.responseText.search("<title>") + 7;
+                end = xmlHttpRequest.responseText.search("</title>");
+                errorMsg =  " On Simpan ";
+                if (start > 0 && end > 0)
+                    alert("Rangerti "+errorMsg + "  [" + xmlHttpRequest.responseText.substring(start, end) + "]");
+                else
+                    alert("Error juga "+errorMsg);
+            }
+        });
+    });
+} 
 
-         function dohapus(edidx,edNoIdentitas){ 
-         if (confirm("Anda yakin Akan menghapus data "+edNoIdentitas+"?")) 
-     { 
-         $(document).ready(function(){ 
-           $.ajax({ 
-                 url: getBaseURL()+"index.php/ctrdenda/deletetable/",
-                 data: "edidx="+edidx, 
-                 cache: false, 
-                 dataType: 'json', 
-                 type: 'POST', 
-                 success: function(json){ 
+function dohapus(edidx,edNoIdentitas){ 
+    if (confirm("Anda yakin Akan menghapus data "+edNoIdentitas+"?"))
+    {
+        $(document).ready(function(){
+            $.ajax({
+                url: getBaseURL()+"index.php/ctrdenda/deletetable/",
+                data: "edidx="+edidx,
+                cache: false,
+                dataType: 'json',
+                type: 'POST',
+                success: function(json){
                     doClear(); 
                     dosearch('-99'); 
-                 }, 
-               error: function (xmlHttpRequest, textStatus, errorThrown) { 
-                     start = xmlHttpRequest.responseText.search("<title>") + 7; 
-                     end = xmlHttpRequest.responseText.search("</title>"); 
-                     errorMsg = " HAPUS "; 
-                     if (start > 0 && end > 0) 
-                         alert("Rangerti "+errorMsg + "  [" + xmlHttpRequest.responseText.substring(start, end) + "]"); 
-                     else 
-                         alert("Error juga "+errorMsg); 
-               } 
-           }); 
-         }); 
-        } 
-        } 
+                },
+                error: function (xmlHttpRequest, textStatus, errorThrown) {
+                    start = xmlHttpRequest.responseText.search("<title>") + 7;
+                    end = xmlHttpRequest.responseText.search("</title>");
+                    errorMsg = " HAPUS ";
+                    if (start > 0 && end > 0)
+                        alert("Rangerti "+errorMsg + "  [" + xmlHttpRequest.responseText.substring(start, end) + "]");
+                    else
+                        alert("Error juga "+errorMsg);
+                }
+            });
+        });
+    }
+} 
 
 
-     function initCorners() { 
-             var setting = { 
-                 tl: { radius: 10 }, // top left 
-                 tr: { radius: 10 }, // top right 
-                 bl: { radius: 6 }, // bottom left 
-                 br: { radius: 6 }, // bottom right 
-                 antiAlias: true 
-             } 
-           curvyCorners(setting, "div#mnhead h1"); 
-          } 
-     addEvent(window, 'load', initCorners); 
-     dosearch(0); 
+function initCorners() { 
+    var setting = {
+        tl: {
+            radius: 10
+        }, // top left
+        tr: {
+            radius: 10
+        }, // top right
+        bl: {
+            radius: 6
+        }, // bottom left
+        br: {
+            radius: 6
+        }, // bottom right
+        antiAlias: true
+    }
+    curvyCorners(setting, "div#mnhead h1");
+} 
+addEvent(window, 'load', initCorners); 
+dosearch(0); 
+function strpad(val){
+    return (!isNaN(val) && val.toString().length==1)?"0"+val:val;
+}
+
+function settanggal(){
+    
+    $(document).ready(function() {
+        var currentTimeAndDate = new Date();
+        var Date30 = new Date();
+        var date = new Date();
+        Date30.setDate(Date30.getDate()-30);
 
 
+
+        var dd = date.getDate();
+        var mm = date.getMonth();
+        var yy = date.getYear();
+
+        var hh = date.getHours();
+        var mnt = date.getMinutes();
+
+        var dd30 = Date30.getDate();
+        var mm30 = Date30.getMonth();
+        var yy30 = Date30.getYear();
+
+        yy  = (yy < 1000) ? yy + 1900 : yy;
+
+
+
+        $("#stylized input#edtgldenda" ).datepicker({
+            dateFormat: 'yy-mm-dd'
+        });
+        $("#stylized input#edtgldenda" ).val(yy+"-"+strpad(mm+1)+"-"+strpad(dd));
+
+
+
+
+    });  
+}
+
+function setnominal(){
+    $(document).ready(function() {
+        $("#stylized input#edDenda").autoNumeric();
+        $("#stylized input#edDendaSparta").autoNumeric();
+    //$("#ednominal").autoNumeric();
+    });
+}
+
+settanggal();
+setnominal();
