9a81d75cff34eaae8f73c69f1770c32b273a0944
diarez77 <diarez77@gmail.com>
2011-04-13 07:59:13 +0000

    okkkk

--- keuanganperpus/application/models/modeldenda.php
+++ keuanganperpus/application/models/modeldenda.php
@@ -1,125 +1,228 @@
-<?php
-
-if (!defined('BASEPATH'))
-    exit('Tidak Diperkenankan mengakses langsung');
-/* Class  Model : anggotabaca  * di Buat oleh Diar PHP Generator * Update List untuk grid karena program generatorku lom sempurna ya hehehehehe */
-
+<?php if(!defined('BASEPATH')) exit('Tidak Diperkenankan mengakses langsung');
+/* Class  Model : transaksi  * di Buat oleh Diar PHP Generator * Update List untuk grid karena program generatorku lom sempurna ya hehehehehe */
 class modeldenda extends CI_Model {
+  function __construct()
+ {
+    parent::__construct();
+ }
 
-    function __construct() {
-        parent::__construct();
-    }
+//****************update 28 maret 2011**********
+ function setDeletetransaksianggotabaca($xidx)
+{
+ $xStr =  " DELETE FROM transaksi WHERE transaksi.idpegawai = '".$xidx."'";
 
-    function getArrayListanggotabaca() { /* spertinya perlu lock table */
-        $xBuffResul = array();
-        $xStr = "SELECT " .
-                "idx," .
-                "NoIdentitas," .
-                "Nama," .
-                "idJenisAnggota," .
-                "Alamat," .
-                "Kota," .
-                "kodepos," .
-                "Notelp," .
-                "email" .
-                " FROM anggotabaca   order by idx ASC ";
-        $query = $this->db->query($xStr);
-        foreach ($query->result() as $row) {
-            $xBuffResul[$row->idx] = $row->NoIdentitas;
-        }
-        return $xBuffResul;
-    }
+ $query = $this->db->query($xStr);
+}
 
-    function getListanggotabaca($xAwal, $xLimit, $xSearch='') {
-        if (!empty($xSearch)) {
-            $xSearch = "Where NoIdentitas like '%" . $xSearch . "%'";
-        }
-        $xStr = "SELECT " .
-                "idx," .
-                "NoIdentitas," .
-                "Nama," .
-                "idJenisAnggota," .
-                "Alamat," .
-                "Kota," .
-                "kodepos," .
-                "Notelp," .
-                "email" .
-                " FROM anggotabaca $xSearch order by idx DESC limit " . $xAwal . "," . $xLimit;
-        $query = $this->db->query($xStr);
-        return $query;
-    }
+Function setInserttransaksidenda($xidx,$xidplu,$xidjenistransaksi,$xidpegawai,$xidunitkerja,
+                              $xidstatusdinas,$xjumlahsatuan,$xnominalpersatuan,
+                              $xtotal,$xiduser,$xnominaldenda,$xiddendasparta,$xidlokasi)
+{
+  $xStr =  " INSERT INTO transaksi( ".
+              "idx,".
+              "idplu,".
+              "idjenistransaksi,".
+              "idpegawai,".
+              "idunitkerja,".
+              "idstatusdinas,".
+              "tanggal,".
+              "jam,".
+              "jumlahsatuan,".
+              "nominalpersatuan,".
+              "total,".
+              "iduser,".
+              "nominaldenda,".
+              "iddendasparta,".
+              "idlokasi) VALUES('".$xidx."','".$xidplu."','".$xidjenistransaksi."','".$xidpegawai."','".$xidunitkerja."','".$xidstatusdinas."',current_date,current_time,'".$xjumlahsatuan."','".$xnominalpersatuan."','".$xtotal."','".$xiduser."','".$xnominaldenda."','".$xiddendasparta."','".$xidlokasi."')";
 
-    function getDetailanggotabaca($xidx) {
-        $xStr = "SELECT " .
-                "idx," .
-                "NoIdentitas," .
-                "Nama," .
-                "idJenisAnggota," .
-                "Alamat," .
-                "Kota," .
-                "kodepos," .
-                "Notelp," .
-                "email" .
-                " FROM anggotabaca  WHERE idx = '" . $xidx . "'";
-
-        $query = $this->db->query($xStr);
-        $row = $query->row();
-        return $row;
-    }
-
-    function getLastIndexanggotabaca() { /* spertinya perlu lock table */
-        $xStr = "SELECT " .
-                "idx," .
-                "NoIdentitas," .
-                "Nama," .
-                "idJenisAnggota," .
-                "Alamat," .
-                "Kota," .
-                "kodepos," .
-                "Notelp," .
-                "email" .
-                " FROM anggotabaca order by idx DESC limit 1 ";
-        $query = $this->db->query($xStr);
-        $row = $query->row();
-        return $row;
-    }
-
-    Function setInsertanggotabaca($xidx, $xNoIdentitas, $xNama, $xidJenisAnggota, $xAlamat, $xKota, $xkodepos, $xNotelp, $xemail) {
-        $xStr = " INSERT INTO anggotabaca( " .
-                "idx," .
-                "NoIdentitas," .
-                "Nama," .
-                "idJenisAnggota," .
-                "Alamat," .
-                "Kota," .
-                "kodepos," .
-                "Notelp," .
-                "email) VALUES('" . $xidx . "','" . $xNoIdentitas . "','" . $xNama . "','" . $xidJenisAnggota . "','" . $xAlamat . "','" . $xKota . "','" . $xkodepos . "','" . $xNotelp . "','" . $xemail . "')";
-        $query = $this->db->query($xStr);
-        return $xidx;
-    }
-
-    Function setUpdateanggotabaca($xidx, $xNoIdentitas, $xNama, $xidJenisAnggota, $xAlamat, $xKota, $xkodepos, $xNotelp, $xemail) {
-        $xStr = " UPDATE anggotabaca SET " .
-                "idx='" . $xidx . "'," .
-                "NoIdentitas='" . $xNoIdentitas . "'," .
-                "Nama='" . $xNama . "'," .
-                "idJenisAnggota='" . $xidJenisAnggota . "'," .
-                "Alamat='" . $xAlamat . "'," .
-                "Kota='" . $xKota . "'," .
-                "kodepos='" . $xkodepos . "'," .
-                "Notelp='" . $xNotelp . "'," .
-                "email='" . $xemail . "' WHERE idx = '" . $xidx . "'";
-        $query = $this->db->query($xStr);
-        return $xidx;
-    }
-
-    function setDeleteanggotabaca($xidx) {
-        $xStr = " DELETE FROM anggotabaca WHERE anggotabaca.idx = '" . $xidx . "'";
-
-        $query = $this->db->query($xStr);
-    }
+// return $xStr;
+$query = $this->db->query($xStr);
+return $xidx;
 
 }
 
+
+ Function setUpdatetransaksidenda($xidx,$xidplu,$xidjenistransaksi,$xidpegawai,$xidunitkerja,$xidstatusdinas,$xjumlahsatuan,
+                               $xnominalpersatuan,$xtotal,$xiduser,$xnominaldenda,$xiddendasparta,$xidlokasi)
+{
+  $xStr =  " UPDATE transaksi SET ".
+             "idx='".$xidx."',".
+             "idplu='".$xidplu."',".
+             "idjenistransaksi='".$xidjenistransaksi."',".
+             "idpegawai='".$xidpegawai."',".
+             "idunitkerja='".$xidunitkerja."',".
+             "idstatusdinas='".$xidstatusdinas."',".
+             "tanggal=current_date,".
+             "jam=current_time,".
+             "jumlahsatuan='".$xjumlahsatuan."',".
+             "nominalpersatuan='".$xnominalpersatuan."',".
+             "total='".$xtotal."',".
+             "iduser='".$xiduser."',".
+             "nominaldenda='".$xnominaldenda."',".
+             "iddendasparta='".$xiddendasparta."',".
+             "idlokasi='".$xidlokasi."' WHERE idx = '".$xidx."'";
+ $query = $this->db->query($xStr);
+ return $xidx;
+}
+//**
+
+
+function getArrayListtransaksi(){ /* spertinya perlu lock table*/
+ $xBuffResul = array();
+ $xStr =  "SELECT ".
+      "idx,".
+      "idplu,".
+      "idjenistransaksi,".
+      "idpegawai,".
+      "idunitkerja,".
+      "idstatusdinas,".
+      "tanggal,".
+      "jam,".
+      "jumlahsatuan,".
+      "nominalpersatuan,".
+      "total,".
+      "iduser,".
+      "nominaldenda,".
+      "iddendasparta,".
+      "idlokasi".
+" FROM transaksi   order by idx ASC ";
+ $query = $this->db->query($xStr);
+ foreach ($query->result() as $row)
+ {
+   $xBuffResul[$row->idx] = $row->idplu;
+   }
+return $xBuffResul;
+}
+
+function getListtransaksi($xAwal,$xLimit,$xSearch=''){
+if(!empty($xSearch)){
+     $xSearch = "Where idplu like '%".$xSearch."%'" ;
+ }
+ $xStr =   "SELECT ".
+      "idx,".
+      "idplu,".
+      "idjenistransaksi,".
+      "idpegawai,".
+      "idunitkerja,".
+      "idstatusdinas,".
+      "tanggal,".
+      "jam,".
+      "jumlahsatuan,".
+      "nominalpersatuan,".
+      "total,".
+      "iduser,".
+      "nominaldenda,".
+      "iddendasparta,".
+      "idlokasi ".
+" FROM transaksi $xSearch order by idx DESC limit ".$xAwal.",".$xLimit;
+ $query = $this->db->query($xStr);
+ return $query ;
+}
+
+
+function getDetailtransaksi($xidx){
+ $xStr =   "SELECT ".
+      "idx,".
+      "idplu,".
+      "idjenistransaksi,".
+      "idpegawai,".
+      "idunitkerja,".
+      "idstatusdinas,".
+      "tanggal,".
+      "jam,".
+      "jumlahsatuan,".
+      "nominalpersatuan,".
+      "total,".
+      "iduser,".
+      "nominaldenda,".
+      "iddendasparta,".
+      "idlokasi".
+" FROM transaksi  WHERE idx = '".$xidx."'";
+
+ $query = $this->db->query($xStr);
+$row = $query->row();
+ return $row;
+}
+
+
+function getLastIndextransaksi(){ /* spertinya perlu lock table*/
+ $xStr =   "SELECT ".
+      "idx,".
+      "idplu,".
+      "idjenistransaksi,".
+      "idpegawai,".
+      "idunitkerja,".
+      "idstatusdinas,".
+      "tanggal,".
+      "jam,".
+      "jumlahsatuan,".
+      "nominalpersatuan,".
+      "total,".
+      "iduser,".
+      "nominaldenda,".
+      "iddendasparta,".
+      "idlokasi".
+" FROM transaksi order by idx DESC limit 1 ";
+ $query = $this->db->query($xStr);
+$row = $query->row();
+ return $row;
+}
+
+
+ Function setInserttransaksi($xidx,$xidplu,$xidjenistransaksi,$xidpegawai,$xidunitkerja,$xidstatusdinas,$xtanggal,$xjam,$xjumlahsatuan,$xnominalpersatuan,$xtotal,$xiduser,$xnominaldenda,$xiddendasparta,$xidlokasi)
+{
+  $xStr =  " INSERT INTO transaksi( ".
+              "idx,".
+              "idplu,".
+              "idjenistransaksi,".
+              "idpegawai,".
+              "idunitkerja,".
+              "idstatusdinas,".
+              "tanggal,".
+              "jam,".
+              "jumlahsatuan,".
+              "nominalpersatuan,".
+              "total,".
+              "iduser,".
+              "nominaldenda,".
+              "iddendasparta,".
+              "idlokasi) VALUES('".$xidx."','".$xidplu."','".$xidjenistransaksi."','".$xidpegawai."','".$xidunitkerja."','".$xidstatusdinas."','".$xtanggal."','".$xjam."','".$xjumlahsatuan."','".$xnominalpersatuan."','".$xtotal."','".$xiduser."','".$xnominaldenda."','".$xiddendasparta."','".$xidlokasi."')";
+
+ //echo $xStr;
+$query = $this->db->query($xStr);
+ return $xidx;
+}
+
+
+ Function setUpdatetransaksi($xidx,$xidplu,$xidjenistransaksi,$xidpegawai,$xidunitkerja,$xidstatusdinas,$xtanggal,$xjam,$xjumlahsatuan,$xnominalpersatuan,$xtotal,$xiduser,$xnominaldenda,$xiddendasparta,$xidlokasi)
+{
+  $xStr =  " UPDATE transaksi SET ".
+             "idx='".$xidx."',".
+             "idplu='".$xidplu."',".
+             "idjenistransaksi='".$xidjenistransaksi."',".
+             "idpegawai='".$xidpegawai."',".
+             "idunitkerja='".$xidunitkerja."',".
+             "idstatusdinas='".$xidstatusdinas."',".
+             "tanggal='".$xtanggal."',".
+             "jam='".$xjam."',".
+             "jumlahsatuan='".$xjumlahsatuan."',".
+             "nominalpersatuan='".$xnominalpersatuan."',".
+             "total='".$xtotal."',".
+             "iduser='".$xiduser."',".
+             "nominaldenda='".$xnominaldenda."',".
+             "iddendasparta='".$xiddendasparta."',".
+             "idlokasi='".$xidlokasi."' WHERE idx = '".$xidx."'";
+ $query = $this->db->query($xStr);
+ return $xidx;
+}
+
+
+ function setDeletetransaksi($xidx)
+{
+ $xStr =  " DELETE FROM transaksi WHERE transaksi.idx = '".$xidx."'";
+
+ $query = $this->db->query($xStr);
+}
+
+}
 ?>
