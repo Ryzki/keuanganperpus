fef4faf41dba7f67a08f9e2e05216e644c29b0d5
diarez77 <diarez77@gmail.com>
2011-04-14 18:10:44 +0000

    okkkk

--- keuanganperpus/application/controllers/ctrprodukplu.php
+++ keuanganperpus/application/controllers/ctrprodukplu.php
@@ -22,20 +22,11 @@ class ctrprodukplu extends CI_Controller {
         $this->load->helper('form');
         $this->load->helper('html');
         $this->load->model('modelgetmenu');
-        $xForm = '<div id="stylized" class="myform"><h2>Pengisian Data Produk/PLU </h2>' . form_open_multipart('ctrprodukplu/inserttable', array('id' => 'form', 'name' => 'form')).'<div class="garis"></div>';
-        $xAddJs = '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/js/tiny_mce/jquery.tinymce.js"></script>' .
-                '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/js/thickbox.js"></script>' .
-                '<link rel="stylesheet" href="' . base_url() . 'resource/css/thickbox.css" type="text/css" media="screen" />' .
-                link_tag('resource/css/screenshot.css') .
-                link_tag('resource/js/uploadify/uploadify.css') .
-                '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/js/uploadify/swfobject.js"></script>' .
-                '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/js/uploadify/jquery.uploadify.v2.1.4.js"></script>' .
-                '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/ajax/baseurl.js"></script>' .
+        $xForm = '<div id="stylized" class="myform"><h2>Pengisian Data Produk/PLU </h2>' . form_open_multipart('ctrprodukplu/inserttable', array('id' => 'form', 'name' => 'form')) . '<div class="garis"></div>';
+        $xAddJs = '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/ajax/baseurl.js"></script>' .
                 '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/ajax/ajaxprodukplu.js"></script>' .
-                '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/ajax/ajaxuploadfy.js"></script>' .
                 '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/js/autoNumeric.js"></script>' .
-                '<script language="javascript" type="text/javascript" src="' . base_url() . 'resource/ajax/ajaxmce.js"></script>'.
-                    '<script  type=text/javascript>
+                '<script  type=text/javascript>
                             $(function() {
                              $("#stylized input#edharga").autoNumeric();
                              //$("#edharga").autoNumeric();
@@ -76,14 +67,14 @@ class ctrprodukplu extends CI_Controller {
         $this->load->model('modelrekanan');
         $this->load->model('modeljenistransaksi');
         $xBufResult = '<input type="hidden" name="edidx" id="edidx" value="0" />' .
-        $xBufResult .= setForm('edKodePLU', 'KodePLU', form_input(getArrayObj('edKodePLU', $xKodePLU, '100')));
-        $xBufResult .= setForm('edidJnsPengguna', 'Grouping Pengguna',  form_dropdown('edidJnsPengguna', $this->modeljenipengguna->getArrayListjenipengguna(),'0','id="edidJnsPengguna" width="150px"')) . '<div class="spacer"></div>';
+                $xBufResult .= setForm('edKodePLU', 'KodePLU', form_input(getArrayObj('edKodePLU', $xKodePLU, '100')));
+        $xBufResult .= setForm('edidJnsPengguna', 'Grouping Pengguna', form_dropdown('edidJnsPengguna', $this->modeljenipengguna->getArrayListjenipengguna(), '0', 'id="edidJnsPengguna" width="150px"')) . '<div class="spacer"></div>';
         $xBufResult .= setForm('edNamaProduk', 'NamaProduk', form_input(getArrayObj('edNamaProduk', $xNamaProduk, '300'))) . '<div class="spacer"></div>';
-        $xBufResult .= setForm('edSingkatan', 'Singkatan', form_input(getArrayObj('edSingkatan', $xSingkatan, '100')),'Cth:A3,A4,Jilid,Print') . '<div class="spacer"></div>';
-        $xBufResult .= setForm('edidstatusPLU', 'Status PLU', form_dropdown('edidstatusPLU', $this->modelstatusplu->getArrayListstatusplu(),'0','id="edidstatusPLU" width="150px"')). '<div class="spacer"></div>';
-        $xBufResult .= setForm('edidrekanan', 'Rekanan', form_dropdown('edidrekanan', $this->modelrekanan->getArrayListrekanan(),'0','id="edidrekanan" width="150px"')) . '<div class="spacer"></div>';
-        $xBufResult .= setForm('edharga', 'Harga', form_input(getArrayObj('edharga', $xharga, '100'))). '<div class="spacer"></div>';
-        $xBufResult .= setForm('edidjenistransaksi', 'Jenistransaksi', form_dropdown('edidjenistransaksi', $this->modeljenistransaksi->getArrayListjenistransaksi(),'0','id="edidjenistransaksi" width="150px"')) . '<div class="spacer"></div>';
+        $xBufResult .= setForm('edSingkatan', 'Singkatan', form_input(getArrayObj('edSingkatan', $xSingkatan, '100')), 'Cth:A3,A4,Jilid,Print') . '<div class="spacer"></div>';
+        $xBufResult .= setForm('edidstatusPLU', 'Status PLU', form_dropdown('edidstatusPLU', $this->modelstatusplu->getArrayListstatusplu(), '0', 'id="edidstatusPLU" width="150px"')) . '<div class="spacer"></div>';
+        $xBufResult .= setForm('edidrekanan', 'Rekanan', form_dropdown('edidrekanan', $this->modelrekanan->getArrayListrekanan(), '0', 'id="edidrekanan" width="150px"')) . '<div class="spacer"></div>';
+        $xBufResult .= setForm('edharga', 'Harga', form_input(getArrayObj('edharga', $xharga, '100'))) . '<div class="spacer"></div>';
+        $xBufResult .= setForm('edidjenistransaksi', 'Jenistransaksi', form_dropdown('edidjenistransaksi', $this->modeljenistransaksi->getArrayListjenistransaksi(), '0', 'id="edidjenistransaksi" width="150px"')) . '<div class="spacer"></div>';
         $xBufResult .= '<div class="garis"></div>' . form_button('btSimpan', 'simpan', 'onclick="dosimpan();"') . form_button('btNew', 'new', 'onclick="doClear();"') . '<div class="spacer"></div>';
         return $xBufResult;
     }
@@ -125,7 +116,6 @@ class ctrprodukplu extends CI_Controller {
             $xButtonHapus = '<img src="' . base_url() . 'resource/imgbtn/delete_table.png" alt="Hapus Data" onclick = "dohapus(\'' . $row->idx . '\',\'' . substr($row->KodePLU, 0, 20) . '\');" style="border:none;">';
             $xbufResult .= addRow(addCell($row->idx, 'width:100px;') .
                             addCell($row->KodePLU, 'width:100px;') .
-                            
                             addCell($row->NamaProduk, 'width:100px;') .
                             addCell($row->harga, 'width:100px;') .
                             addCell($row->idjenistransaksi, 'width:100px;') .
@@ -139,8 +129,7 @@ class ctrprodukplu extends CI_Controller {
         $xRowCells = addCell($xButtonADD, 'width:100px;', true) .
                 addCell($xInput, 'width:200px;border-right:0px;', true) .
                 addCell($xButtonSearch, 'width:40px;border-right:0px;border-left:0px;', true) .
-                addCell($xButtonPrev . '&nbsp&nbsp' . $xButtonNext, 'width:100px;border-left:0px;', true)
-        ;
+                addCell($xButtonPrev . '&nbsp&nbsp' . $xButtonNext, 'width:100px;border-left:0px;', true);
         return '<div id="tabledata" name ="tabledata" class="tc1" style="width:650px;">' . $xbufResult . $xRowCells . '</div>';
     }
 
@@ -217,9 +206,9 @@ class ctrprodukplu extends CI_Controller {
         $xidjenistransaksi = $_POST['edidjenistransaksi'];
         $this->load->model('modelprodukplu');
         if ($xidx != '0') {
-            $xStr = $this->modelprodukplu->setUpdateprodukplu($xidx, $xKodePLU, $xidJnsPengguna, $xNamaProduk, $xSingkatan, $xidstatusPLU, $xidrekanan, str_replace('.','',$xharga) , $xidjenistransaksi);
+            $xStr = $this->modelprodukplu->setUpdateprodukplu($xidx, $xKodePLU, $xidJnsPengguna, $xNamaProduk, $xSingkatan, $xidstatusPLU, $xidrekanan, str_replace('.', '', $xharga), $xidjenistransaksi);
         } else {
-            $xStr = $this->modelprodukplu->setInsertprodukplu($xidx, $xKodePLU, $xidJnsPengguna, $xNamaProduk, $xSingkatan, $xidstatusPLU, $xidrekanan, str_replace('.','',$xharga), $xidjenistransaksi);
+            $xStr = $this->modelprodukplu->setInsertprodukplu($xidx, $xKodePLU, $xidJnsPengguna, $xNamaProduk, $xSingkatan, $xidstatusPLU, $xidrekanan, str_replace('.', '', $xharga), $xidjenistransaksi);
         }
     }
 
